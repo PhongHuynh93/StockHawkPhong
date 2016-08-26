96872a0b090b8ae2704b6b59def7502634babe01
Phong <p7157568@gmail.com>
2016-08-26 08:32:14 +0700

    feat - make networkUtil

--- app/src/main/java/dhbk/android/stockhawk/SyncService.java
+++ app/src/main/java/dhbk/android/stockhawk/SyncService.java
@@ -1,8 +1,13 @@
 package dhbk.android.stockhawk;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.IBinder;
+
+import dhbk.android.stockhawk.util.NetworkUtil;
 
 public class SyncService extends Service {
     public SyncService() {
@@ -12,5 +17,20 @@ public class SyncService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     *  5 - declare broadcastreceiver
+     */
+    public static class SyncOnConnectionAvailable extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)
+                    && NetworkUtil.isNetworkConnected(context)) {
+                Timber.i("Connection is now available, triggering sync...");
+                AndroidComponentUtil.toggleComponent(context, this.getClass(), false);
+                context.startService(getStartIntent(context));
+            }
+        }
     }
 }
---
+++ app/src/main/java/dhbk/android/stockhawk/util/NetworkUtil.java
@@ -0,0 +1,37 @@
+package dhbk.android.stockhawk.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import retrofit2.adapter.rxjava.HttpException;
+
+/**
+ * Created by huynhducthanhphong on 8/26/16.
+ */
+public class NetworkUtil {
+    /**
+     * Returns true if the Throwable is an instance of RetrofitError with an
+     * http status code equals to the given one.
+     * @param throwable
+     * @param statusCode
+     * @return the error has appear or not
+     */
+    public static boolean isHttpStatusCode(Throwable throwable, int statusCode) {
+        return throwable instanceof HttpException
+                && ((HttpException) throwable).code() == statusCode;
+    }
+
+    /**
+     * return true if at least one network active
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkConnected(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+}
+
+
