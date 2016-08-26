fc93823f41455cd55bd502197dc1cbd57b66ca60
Phong <p7157568@gmail.com>
2016-08-26 22:38:52 +0700

    feat - add BasePresenter

--- app/src/main/java/dhbk/android/stockhawk/ui/main/MainPresenter.java
+++ app/src/main/java/dhbk/android/stockhawk/ui/main/MainPresenter.java
@@ -1,7 +1,10 @@
 package dhbk.android.stockhawk.ui.main;
 
+import dhbk.android.stockhawk.ui.base.BasePresenter;
+
 /**
  * Created by huynhducthanhphong on 8/26/16.
  */
-public class MainPresenter extends BasePresenter<MainMvpView>{
+public class MainPresenter extends BasePresenter<MainMvpView> {
+
 }
