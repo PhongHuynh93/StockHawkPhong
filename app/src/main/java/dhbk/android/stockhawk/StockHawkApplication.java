bd624cd439057c314fc2e4c047846c9d1c50e935
Phong <p7157568@gmail.com>
2016-08-26 10:15:20 +0700

    feat - add get and set application component

--- app/src/main/java/dhbk/android/stockhawk/StockHawkApplication.java
+++ app/src/main/java/dhbk/android/stockhawk/StockHawkApplication.java
@@ -7,8 +7,15 @@ import timber.log.Timber;
 
 /**
  * Created by huynhducthanhphong on 8/26/16.
+ * <p/>
+ * declare appliation
  */
-public class StockHawkApplication extends Application{
+public class StockHawkApplication extends Application {
+    ApplicationComponent mApplicationComponent;
+
+    /**
+     * initialize the library use in application
+     */
     @Override
     public void onCreate() {
         super.onCreate();
@@ -16,19 +23,41 @@ public class StockHawkApplication extends Application{
 //        1 declare library
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
+            Fabric.with(this, new Crashlytics());
         }
 
+        FlowManager.init(new FlowConfig.Builder(this).build());
+        Stetho.initializeWithDefaults(this);
     }
 
-
-    //    2 get this application
-
     /**
+     * 2 get this application
      *
      * @param context a class call this method must have a context
      * @return
      */
     public static StockHawkApplication get(Context context) {
         return (StockHawkApplication) context.getApplicationContext();
+    }
+
+    /**
+     * get application component
+     *
+     * @return
+     */
+    public ApplicationComponent getComponent() {
+        if (mApplicationComponent == null) {
+            mApplicationComponent = DaggerApplicationComponent
+                    .builder()
+                    .applicationModule(new ApplicationModule(this))
+                    .build();
+        }
+        return mApplicationComponent;
+    }
+
+
+    // Needed to replace the component with a test specific one
+    public void setComponent(ApplicationComponent applicationComponent) {
+        mApplicationComponent = applicationComponent;
     }
 }
