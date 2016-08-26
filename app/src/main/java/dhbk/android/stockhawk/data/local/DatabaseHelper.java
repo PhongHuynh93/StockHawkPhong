2107bd9a8db8273bdcdc2454fd7f76b1b214ed46
Phong <p7157568@gmail.com>
2016-08-26 15:44:07 +0700

    feat - learn how to save/ get db from DBFlow

--- app/src/main/java/dhbk/android/stockhawk/data/local/DatabaseHelper.java
+++ app/src/main/java/dhbk/android/stockhawk/data/local/DatabaseHelper.java
@@ -2,6 +2,7 @@ package dhbk.android.stockhawk.data.local;
 
 import com.raizlabs.android.dbflow.sql.language.Delete;
 import com.raizlabs.android.dbflow.sql.language.SQLite;
+import com.raizlabs.android.dbflow.structure.BaseModel;
 
 import java.util.List;
 
@@ -13,7 +14,6 @@ import dhbk.android.stockhawk.data.model.Quote_Table;
 import dhbk.android.stockhawk.data.model.multiple.Stocks;
 import dhbk.android.stockhawk.data.model.single.Stock;
 import rx.Observable;
-import rx.functions.Action1;
 import rx.functions.Func0;
 
 @Singleton
@@ -27,6 +27,7 @@ public class DatabaseHelper {
     }
 
     /**
+     * save stock to db
      *
      * @param stock
      * @return
@@ -35,65 +36,58 @@ public class DatabaseHelper {
         /**
          * do not create the Observable until the observer subscribes, and create a fresh Observable for each observer
          * @see <a href="http://reactivex.io/documentation/operators/defer.html"></a>
+         *
+         * do not save quote immediately, but wait when the observer attach to this
          */
-        return Observable.defer(new Func0<Observable<Stocks>>() {
-            @Override
-            public Observable<Stocks> call() {
-                List<Quote> quote = stock.getQuery().getResult().getQuote();
-                Observable.from(quote)
-                        .subscribe(new Action1<Quote>() {
-                            @Override
-                            public void call(Quote quote) {
-                                quote.save();
-                            }
-                        });
+        return Observable.defer(() -> {
+            List<Quote> quote = stock.getQuery().getResult().getQuote();
 
-                return Observable.just(stock);
-            }
+            // fixme need only onNext method, save a list of stock to db
+            // to save table, simply save()
+            Observable.from(quote)
+                    .subscribe(BaseModel::save);
+
+            return Observable.just(stock);
         });
     }
 
     /**
      *
+     * fixme ssave one stock to db
      * @param stock
      * @return
      */
     public Observable<Stock> setStock(final Stock stock) {
-        return Observable.defer(new Func0<Observable<Stock>>() {
-            @Override
-            public Observable<Stock> call() {
-
-                Quote quote = stock.getQuery().getResult().getQuote();
-                quote.save();
-
-                return Observable.just(stock);
-            }
+        return Observable.defer(() -> {
+            Quote quote = stock.getQuery().getResult().getQuote();
+            quote.save();
+            return Observable.just(stock);
         });
     }
 
     /**
      *
+     * get stock from db
      * @return
      */
     public Observable<Stocks> getStocks() {
-        return Observable.defer(new Func0<Observable<Stocks>>() {
-            @Override
-            public Observable<Stocks> call() {
+        return Observable.defer(() -> {
 
-                List<Quote> quotes = SQLite.select()
-                        .from(Quote.class)
-                        .queryList();
+            /**
+             * fixme - read all contains in queryList
+             */
+            List<Quote> quotes = SQLite.select()
+                    .from(Quote.class)
+                    .queryList();
 
-                Stocks stock = new Stocks();
-                stock.getQuery().getResult().setQuote(quotes);
+            Stocks stock = new Stocks();
+            stock.getQuery().getResult().setQuote(quotes);
 
-                return Observable.just(stock);
-            }
+            return Observable.just(stock);
         });
     }
 
     /**
-     *
      * @param symbol
      * @return
      */
@@ -102,6 +96,7 @@ public class DatabaseHelper {
             @Override
             public Observable<Stocks> call() {
 
+                // FIXME: 8/26/16 delete the table quote
                 Delete.table(Quote.class,
                         Quote_Table.msymbol.eq(symbol));
 
