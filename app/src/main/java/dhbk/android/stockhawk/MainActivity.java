e46f01bb4bd0a8efd7cecb58e45bb95a3808d3d3
Phong <p7157568@gmail.com>
2016-08-26 21:01:11 +0700

    feat - add resources

--- app/src/main/java/dhbk/android/stockhawk/MainActivity.java
+++ app/src/main/java/dhbk/android/stockhawk/MainActivity.java
@@ -1,10 +1,39 @@
 package dhbk.android.stockhawk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String EXTRA_TRIGGER_SYNC_FLAG =
+            "rajan.udacity.stock.hawk.ui.main.MainActivity.EXTRA_TRIGGER_SYNC_FLAG";
+
     // TODO: 8/26/16 inject
+    @Inject
+    MainPresenter mMainPresenter;
+
+    @Inject
+    StockAdapter mStocksAdapter;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    /**
+     * Return an Intent to start this Activity.
+     * triggerDataSyncOnCreate allows disabling the background sync service onCreate. Should
+     * only be set to false during testing.
+     */
+    public static Intent getStartIntent(Context context, boolean triggerDataSyncOnCreate) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(EXTRA_TRIGGER_SYNC_FLAG, triggerDataSyncOnCreate);
+        return intent;
+    }
 
 
     // TODO: 8/26/16 oncreate
@@ -12,5 +41,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
 }
